bcbc42c1c08fca2620cc1d140d716e22d1486401
MK ッ <63323163+CoolGuyMK@users.noreply.github.com>
2022-03-17 00:38:49 +0200

    I'm a barbie girl...
    I'm a Barbie girl in the Barbie world Life in plastic, it's fantastic! You can brush my hair, undress me everywhere Imagination, life is your creation I'm a Barbie girl in the Barbie world Life in plastic, it's fantastic! You can brush my hair, undress me everywhere Imagination, life is your creation Come on, Barbie, let's go party! (Ah-ah-ah-yeah)

--- task.sh
+++ task.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-arr[0]="bot: 😒 Hi..."
-arr[1]="bot: 😑 Boring... "
+arr[0]="bot: 💅 I'm a barbie girl.."
+arr[1]="bot: 👀 Watch out! Watch out! ta tara ta"
 arr[2]="bot: 🥱 Gn.. "
 arr[3]="bot: 🤡 Lakaka"
-arr[4]="bot: 😪 Hufft..."
+arr[4]="bot: 😩 Siuuu"
 arr[5]="bot: 💀 Roberto Levandoksa"
 arr[6]="bot: ඞ Amogus"
 arr[7]="bot: 🤌 Pizza"
@@ -12,8 +12,8 @@ arr[7]="bot: 🤌 Pizza"
 rand=$[$RANDOM % ${#arr[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 
-echo "## 🤔 LAST UPDATED AT: ${d}" > update.md
+echo "## 👀 LAST UPDATED AT: ${d}" > update.md
 
 git config --local user.email "coolguymk1@gmail.com"
 git config --local user.name "CoolGuyMK"
-git commit -am "${arr[$rand]} (at ${d})"
+git commit -am "${arr[$rand]}"
