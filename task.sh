14c53063fa17e2807b7d8ae8fa83160ad7c73867
MK ッ <63323163+CoolGuyMK@users.noreply.github.com>
2022-03-16 22:18:01 +0200

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-arr[0]="bot: 👋 Hello Github!"
-arr[1]="bot: 🥳 Yeayyy!"
-arr[2]="bot: 😬 Working from github."
-arr[3]="bot: 👨‍💻 Work, work, work!"
+arr[0]="bot: 😒 Hi..."
+arr[1]="bot: 😑 Boring... "
+arr[2]="bot: 🥱 Gn.. "
+arr[3]="bot: 🤡 Lakaka"
 arr[4]="bot: 😪 Hufft..."
-arr[5]="bot: 😎 I'm working for my master!"
+arr[5]="bot: 💀 Roberto Levandoksa"
 arr[6]="bot: 🙄 Running task, again."
-arr[7]="bot: 👻 Thanks master."
+arr[7]="bot: 🤌 Pizza"
 
 rand=$[$RANDOM % ${#arr[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 
 echo "## 🤔 LAST UPDATED AT: ${d}" > update.md
 
-git config --local user.email "i.xyzuan@gmail.com"
-git config --local user.name "xyzuan"
+git config --local user.email "coolguymk1@gmail.com"
+git config --local user.name "CoolGuyMK"
 git commit -am "${arr[$rand]} (at ${d})"
